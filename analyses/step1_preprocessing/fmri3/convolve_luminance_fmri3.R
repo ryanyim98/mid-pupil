9d39050333d6fd0a0aa0ed1170ec907029433983
ryany <ryanyim98@gmail.com>
2025-03-04 16:02:44 -0800

    manuscript sent to Brian

--- analyses/step1_preprocessing/fmri3/convolve_luminance_fmri3.R
+++ analyses/step1_preprocessing/fmri3/convolve_luminance_fmri3.R
@@ -11,6 +11,9 @@ for (sub in unique(fmri3_pupil_data$subject)){
   for (t in unique(my.df.temp$trial)){
     my.df.temp.trial <- my.df.temp %>% 
       filter(trial == t)
+    my.df.temp.trial <- my.df.temp.trial %>% 
+      mutate(luminance_deriv = luminance - lag(luminance),
+             luminance_deriv = ifelse(is.na(luminance_deriv),0,luminance_deriv))
     tval <- my.df.temp.trial$sample_in_trial_t
     gamma_pdf_new <- dgamma(seq(0,20,0.005), shape = k_new, scale = theta) #dgamma(t_values, shape = k, scale = theta)
     gamma_pdf_orig<-dgamma(seq(0,20,0.005), shape = k_orig, scale = theta)
@@ -18,6 +21,7 @@ for (sub in unique(fmri3_pupil_data$subject)){
     gamma_pdf_orig <- gamma_pdf_orig / sum(gamma_pdf_orig)  # Normalize to sum to 1
     gamma_pdf_new <- gamma_pdf_new / sum(gamma_pdf_new)  # Normalize to sum to 1
     my.df.temp.trial$convolved_orig <- convolve(my.df.temp.trial$luminance, rev(gamma_pdf_orig), type = "open")[1:(nrow(my.df.temp.trial))]
+    my.df.temp.trial$convolved_new_deriv <- convolve(my.df.temp.trial$luminance_deriv, rev(gamma_pdf_new), type = "open")[1:(nrow(my.df.temp.trial))]
     my.df.temp.trial$convolved_new <- convolve(my.df.temp.trial$luminance,rev(gamma_pdf_new), type = "open")[1:(nrow(my.df.temp.trial))]
     
     # my.df.temp.trial$convolved_orig <- convolve(my.df.temp.trial$luminance, gamma_pdf_orig,type = "open")[1:(nrow(my.df.temp.trial))]
@@ -27,4 +31,5 @@ for (sub in unique(fmri3_pupil_data$subject)){
 }
 fmri3_pupil_data=fmri3_pupil_data.new
 rm(fmri3_pupil_data.new)
+
 write_csv(fmri3_pupil_data,'~/Desktop/VRMID-analysis/mid-pupil/data/fmri3/derivatives/pupillometry_baselineCorrected_with_luminance_convolved.csv')
